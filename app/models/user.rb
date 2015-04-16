d577224494f7b291d300c51c5bc8e30ba11707f3
HarrietM & Piet Geursen <pair+harrietm.pietgeursen@devbootcamp.com>
2015-04-16 17:38:54 +1200

    FOLLOWERS/FOLLOWEES work

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,9 @@ class User < ActiveRecord::Base
     has_secure_password
     validates_uniqueness_of :email, :handle
     validates_presence_of :email, :handle, :name
+    has_many :followee_follows, class_name: 'Follow', foreign_key: 'followee_id'
+    has_many :followers, class_name: 'User', through: :followee_follows
 
+    has_many :follower_follows, class_name: 'Follow', foreign_key: 'follower_id'
+    has_many :followees, class_name: 'User', through: :follower_follows
 end
