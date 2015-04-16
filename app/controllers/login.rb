cff31dcc22098efbcfd767833797dad5c30feac4
HarrietM & Piet Geursen <pair+harrietm.pietgeursen@devbootcamp.com>
2015-04-16 13:28:36 +1200

    Login functional

---
+++ app/controllers/login.rb
@@ -0,0 +1,25 @@
+get '/' do
+  if session[:id]
+    erb :feed
+  else
+    erb :signin
+  end
+end
+
+get '/signup' do
+  erb :signup
+end
+
+post '/signup' do
+  redirect '/'
+end
+
+post '/signin' do
+  user = User.find_by(email: params[:email]).try(:authenticate, params[:password])
+  if user
+    session[:id] = user.id
+  end
+  redirect '/'
+end
+
+
---
+++ db/seeds.rb
@@ -0,0 +1,2 @@
+user = User.new(name: 'harriet', email: 'hsemckinnon@gmail.com', password: 'password', password_confirmation: 'password')
+user.save
