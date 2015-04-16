1bf110dd419b8e445e59c2a0ccb06c1e0984ec41
HarrietM & Piet Geursen <pair+harrietm.pietgeursen@devbootcamp.com>
2015-04-16 15:58:26 +1200

    Validations for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
     has_secure_password
+    validates_uniqueness_of :email, :handle
+    validates_presence_of :email, :handle, :name
 
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,4 +4,19 @@ describe User do
   it "has a secure password" do
     should have_secure_password
   end
+  it "has a unique email address" do
+    should validate_uniqueness_of(:email)
+  end
+  it "has a unique handle" do
+    should validate_uniqueness_of(:handle)
+  end
+  it "has a name" do
+    should validate_presence_of(:name)
+  end
+  it "has a email" do
+    should validate_presence_of(:email)
+  end
+  it "has a handle" do
+    should validate_presence_of(:handle)
+  end
 end
