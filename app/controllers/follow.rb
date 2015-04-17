80cc8eb3268e79f2806a9253f4de89bfe03365fa
HarrietM & Piet Geursen <pair+harrietm.pietgeursen@devbootcamp.com>
2015-04-17 14:36:25 +1200

    Follow/unfollow functional

--- app/controllers/follow.rb
+++ app/controllers/follow.rb
@@ -1,17 +1,20 @@
 get '/users' do
-  erb :users
+  @title = "All Users!"
+  @users = User.all
+
+  erb :"tweets/users"
 end
 
 post '/follow' do
   current_user = User.find(session[:id])
-  to_follow = User.find(params[:user_to_follow])
+  to_follow = User.find(params[:id])
   to_follow.followers << current_user
   redirect '/users'
 end
 
-delete '/follow' do
+post '/unfollow' do
   current_user = User.find(session[:id])
-  unfollow = User.find(params[:user_to_unfollow])
+  unfollow = User.find(params[:id])
   unfollow.followers.delete(current_user)
   redirect '/users'
 end
