0f07c8c9405a92a97dba3c464b8c338e15d8294a
HarrietM & Piet Geursen <pair+harrietm.pietgeursen@devbootcamp.com>
2015-04-17 15:34:20 +1200

    sigin form altered

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+require 'faker'
+
 User.delete_all
 Tweet.delete_all
 
@@ -11,3 +13,10 @@ sleep(1)
 user.tweets << Tweet.create(content: "This is Harriet's tweet")
 
 piet.followers << user
+
+10.times do
+  user = User.create(handle: Faker::Internet.user_name, name: Faker::Name.name, email: Faker::Internet.email, password: 'password', password_confirmation: 'password')
+  10.times do
+    user.tweets << Tweet.create(content: Faker::Hacker.say_something_smart)
+  end
+end
