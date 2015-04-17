f5d7deced5759f54291fe0183841611ca6ccbd07
chhingchhing <chhingchhing.hem@gmail.com>
2015-04-17 12:07:53 +1200

    User page work some

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,15 +1,13 @@
 get '/tweet/new' do
 	@title = "Welcome to Tweet"
-	# current_user_id = session[:id]
-	current_user_id = 1
+	current_user_id = session[:id]
 	@tweets = Tweet.includes(:user).all
 	erb :"tweets/tweet"
 end
 
 post '/new' do
 	@tweet = Tweet.new(content: params[:content])
-	# @tweet.user_id = session[:id]
-	@tweet.user_id = 1
+	@tweet.user_id = session[:id]
 	if @tweet.save
 		session[:message] = {success: true, message: "Success!"}
 		redirect '/tweet/new'
@@ -21,19 +19,26 @@ end
 
 get '/retweet/:id' do
 	@title = "Retweet!"
+	@tweet = Tweet.find_by_id(params[:id])
+	@user = User.find_by_id(@tweet.user_id)
 	erb :"tweets/retweet"
 end
 
 post '/retweet/:id' do
 	tweet = Tweet.find_by_id(params[:id])
-	# user = User.find_by_id(tweet.user_id)
 	tweet.count_retweet = tweet.count_retweet + 1
 	tweet.save
 
-	# current_user_id = session[:id]
-	current_user_id = 1
+	current_user_id = session[:id]
 	# create new record of tweeter
-	retweet = TweeterRetweeter.new(user_id: current_user_id, tweet_id: params[:id])
+	retweet = TweetersRetweeter.new(user_id: current_user_id, tweet_id: params[:id])
 	retweet.save
-	# redirect '/tweet/new'
+	redirect '/tweet/new'
+end
+
+get '/users' do
+	@title = "All User!"
+	@users = User.all
+
+	erb :"tweets/users"
 end
